09bdf56a8f0b6f4b31ab2b1644c5eaa95426ddc5
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-06-10 23:19:46 +0200

    added typescript eslint configs

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,14 +2,8 @@ import js from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const ebarooniBaseESLintConfig = {
-  meta: {
-    name: "@ebarooni/eslint-config-base",
-    version: "1.0.0",
-  },
-  rules: {
-    ...js.configs.recommended,
-    ...eslintPluginPrettierRecommended,
-  },
+  ...js.configs.recommended,
+  ...eslintPluginPrettierRecommended,
 };
 
 export default ebarooniBaseESLintConfig;
---
+++ packages/eslint-config/typescript-eslint-config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+const ebarooniBaseTypeScriptESLintConfig = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...eslintPluginPrettierRecommended,
+);
+
+export default ebarooniBaseTypeScriptESLintConfig;
